f947cabbeb80b8855f0ef999719ede74066ec2c7
Noi Bar <noibarr@gmail.com>
2021-08-20 16:58:01 +0300

    fix chess board calibration

--- calculate_cameras_matrices.py
+++ calculate_cameras_matrices.py
@@ -4,7 +4,6 @@ import pickle
 import os
 from os import path
 import matplotlib.pyplot as plt
-import time
 
 import numpy as np
 from arena import load_arena, file_to_camera
@@ -12,15 +11,19 @@ from camera import Camera
 from utils import init_video
 from coordinate_calculator import CoordinateCalculator
 
-CALIBRATION_POINTS = {
-    Camera.TOP: np.array([(0, 0, 0), (0, 70.4, 0), (99, 70.4, 0), (99, 0, 0)]),
-    Camera.BACK: np.array([(8.85, 2.5, 0), (8.85, 5, 0),(8.85, 7.5, 0), (8.85, 10, 0),
-                           (11.35, 2.5, 0), (11.35, 5, 0), (11.35, 7.5, 0),
-                           (13.85, 2.5, 0), (13.85, 5, 0),
-                           (16.35, 2.5, 0)]),
-    Camera.LEFT: np.array([(0, 0, 0), (0, 24, 0), (0, 70.4, 0), (50, 38, 0), (50, 24, 0)]),
-    Camera.RIGHT: np.array([(0, 0, 0), (0, 24, 0), (0, 70.4, 0), (50, 38, 0), (50, 24, 0)]),
+# Arena configuration values.
+arena_width = 68.3
+arena_length = 97.0
+arena_height = 45
+board_arena_x_gap = 6.35
+board_corner = {
+    'A': (0, 0),
+    'B': (arena_length - board_arena_x_gap, arena_width),
 }
+chess_board_z = 1
+board_x_inner_corners = 9
+board_y_inner_corners = 6
+square_size = 2.5
 
 
 def plot_points(img, points, width, height, color):
@@ -32,10 +35,14 @@ def plot_points(img, points, width, height, color):
         cv2.circle(img, p, 2, color)
 
 
-def plot_arena_axis(camera, frame, world_to_pixels):
-    x_points = [(i, j, 0) for i in range(98) for j in [0, 10, 20, 30, 40, 50, 60, 68.3]]
-    y_points = [(j, i, 0) for i in range(69) for j in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 97]]
-    z_points = [(x, y, i) for i in range(46) for x in [0, 97] for y in [0, 68.3]]
+def plot_arena_axis(frame, world_to_pixels):
+    arena_width_lines = list(range(0, int(arena_width + 1), 10))
+    arena_width_lines.append(arena_width)
+    arena_length_lines = list(range(0, int(arena_length + 1), 10))
+    arena_length_lines.append(arena_length)
+    x_points = [(i, j, 1) for i in range(int(arena_length+1)) for j in arena_width_lines]
+    y_points = [(j, i, 1) for i in range(int(arena_width + 1)) for j in arena_length_lines]
+    z_points = [(x, y, i) for i in range(1, arena_height + 1) for x in [0, arena_length] for y in [0, arena_width]]
 
     x_pixels = [world_to_pixels(x) for x in x_points]
     y_pixels = [world_to_pixels(y) for y in y_points]
@@ -46,48 +53,96 @@ def plot_arena_axis(camera, frame, world_to_pixels):
     plot_points(frame, z_pixels, width, height, (255, 0, 0))
 
 
-def get_camera_matrix_manual(camera, frame, videos_dir, manual_points_file):
-    should_continue = input('Enter yes for manual file')
-    if should_continue == "yes":
-        with open(manual_points_file, 'rb') as file:
-            chosen_points = pickle.load(file)
-            arena_points, chosen_pixels = chosen_points[camera]
-            print(arena_points, chosen_pixels)
-            return calculate_matrix(camera, arena_points, chosen_pixels, frame, videos_dir)
-
-    print('choose 6 arena points with known coordinates')
-    cv2.imshow('frame', frame)
-    cv2.waitKey(0)
+def get_board_visible_points(board, square_size):
+    board_visible = input(f'enter is board {board} visible (yes/no): ') == "yes"
     arena_points = []
-    for i in range(6):
-        coordinates = input(f'enter {i+1} x,y,z').split(',')
-        arena_points.append((float(coordinates[0]),float(coordinates[1]),
-                             float(coordinates[2])))
+    point_names = []
 
+    if not board_visible:
+        return [], []
+
+    possible_inner_corners = [
+            (1, 1), (1, board_y_inner_corners),
+    ]
+    for i in range(2, board_y_inner_corners + 1):
+        possible_inner_corners.append((i, i))
+    possible_inner_corners.extend([
+        (board_x_inner_corners, 1),
+        (board_x_inner_corners, board_y_inner_corners)
+    ])
+
+    all_visible = input(f'is inner corners {possible_inner_corners} in '
+        f'board {board}, are all visible? (yes/no)') == 'yes'
+    for p in possible_inner_corners:
+        i, j = p
+        if not all_visible:
+            visible = input(f'is inner corner ({i},{j}) in board {board}, visible? (yes/no)') == 'yes'
+            if not visible:
+                continue
+        c = get_point_coordinates(1, 1, square_size,
+                                      i, j, board)
+        arena_points.append(c)
+        point_names.append((board, i, j))
+    return arena_points, point_names
+
+
+def get_camera_matrix_manual(camera, frame, videos_dir, manual_points_file):
+    plt.figure(figsize=(12, 8))
+    plt.title(f'camera {camera.value}')
+    plt.imshow(frame)
+    plt.show()
+    arena_points = []
+    point_names = []
+
+    for board in board_corner.keys():
+        b_arena_points, b_point_names = get_board_visible_points(board, square_size)
+        arena_points.extend(b_arena_points)
+        point_names.extend(b_point_names)
+
+    points = select_points(point_names, camera, frame)
+    return calculate_matrix(camera, arena_points, points, frame, videos_dir)
+
+
+def select_points(point_names, camera, frame):
+    cv2.imshow(camera.value, frame)
     points = []
+
     def onclick(event, x, y, flags, params):
         if event != cv2.EVENT_RBUTTONDOWN:
             return
 
         points.append([x, y])
-        if len(points) == len(arena_points):
+        if len(points) == len(point_names):
             cv2.destroyAllWindows()
             return
-        print('right click point {}'.format(arena_points[len(points)]))
+        print('right click point {}'.format(point_names[len(points)]))
 
-    cv2.setMouseCallback('frame', onclick)
-    print('right click point {}'.format(arena_points[len(points)]))
+    cv2.setMouseCallback(camera.value, onclick)
+    print('right click point {}'.format(point_names[len(points)]))
     cv2.waitKey(0)
-    print('selected points: ', points)
-    return calculate_matrix(camera, arena_points, points, frame, videos_dir)
+    cv2.destroyAllWindows()
+    return points
+
+
+def get_point_coordinates(point_zero_line, point_zero_column, square_size,
+                          i, j, board):
+    board_corner_x, board_corner_y = board_corner[board]
+    squares_in_x = i if point_zero_line == 1 else point_zero_line - (i-1)
+    squares_in_y = j if point_zero_column == 1 else point_zero_column - (j-1)
+    board_direction = 1 if board == 'A' else -1
+
+    x = board_corner_x + (squares_in_x * square_size * board_direction)
+    y = board_corner_y + (squares_in_y * square_size * board_direction)
+    z = chess_board_z
+    return x, y, z
 
 
 def calculate_matrix(camera, arena_points, points, frame, videos_dir):
     display_frame = frame.copy()
     cc = CoordinateCalculator(model_points=np.array(arena_points))
     world_to_pixels, r, t = cc.get_world_to_pixels_translation(camera, points)
-    plot_arena_axis(camera, display_frame, world_to_pixels)
-    plt.figure(figsize=(12,8))
+    plot_arena_axis(display_frame, world_to_pixels)
+    plt.figure(figsize=(12, 8))
     plt.imshow(display_frame)
     plt.show()
 
@@ -113,8 +168,6 @@ def get_camera_matrix(videos_dir, video_name, manual_points_file):
     img = frame.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    board_x_inner_corners = 9
-    board_y_inner_corners = 6
     ret, corners = cv2.findChessboardCorners(gray, (board_x_inner_corners, board_y_inner_corners), None)
     chess_corners = []
     chess_3d_corners = []
@@ -129,44 +182,47 @@ def get_camera_matrix(videos_dir, video_name, manual_points_file):
     if ret:
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (9, 6), corners2, ret)
-        cv2.imshow('image with detected chess board', img)
-        cv2.waitKey(0)
+        cv2.drawChessboardCorners(img, (board_x_inner_corners, board_y_inner_corners), corners2, ret)
+        plt.figure(figsize=(12, 8))
+        plt.imshow(img)
+        plt.title(f'image with detected chess board, camera {camera.value}')
+        plt.show()
 
         for i in range(len(corners2)):
             c = corners2[i]
             corner = c.reshape(-1)
             chess_corners.append(tuple(corner))
-            frame = cv2.circle(frame, tuple(corner.astype(int)), 1, (0,0,255),2)
+            frame = cv2.circle(frame, tuple(corner.astype(int)), 1, (0, 0, 255), 2)
             cv2.putText(frame, f'{i}', tuple(corner.astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 100), 2)
 
-        marked_chess_board_file_name = f'chess_board_with_indexes_{camera}.png'
-        marked_chess_board_file_path = "{0}/{1}".format(videos_dir, marked_chess_board_file_name)
-        cv2.imwrite(marked_chess_board_file_path, frame)
-        print('saved indexed chess board to: ', marked_chess_board_file_path)
+        plt.figure(figsize=(12, 8))
+        plt.imshow(frame)
+        plt.show()
+
         print('initiating calibration based on chess board coordinates:')
+        board = input('enter detected board (A for (0,0,0), B otherwise): ')
 
-        board_zero_x = float(input('enter point 0 x in arena'))
-        board_zero_y = float(input('enter point 0 y in arena'))
+        point_zero_line = int(input(f'enter the line (x-axis) of point 0 in the photo (1-{board_x_inner_corners}:'))
+        point_zero_column = int(input(f'enter the column (y-axis) of point 0 in the photo (1-{board_y_inner_corners}:'))
+        square_size = 2.5
 
-        board_row_direction = float(input('is point 0 closer then point 1 to the x zero line? enter 1 if it is, -1 otherwise'))
-        board_column_direction = float(input(f'is point 0 closer then point {board_x_inner_corners} to the y zero line? enter 1 if it is, -1 otherwise'))
+        # corners are ordered row by row, left to right in every row. there are ten rows.
+        for j in range(1, board_y_inner_corners + 1):
+            for i in range(1, board_x_inner_corners + 1):
+                c = get_point_coordinates(point_zero_line, point_zero_column, square_size,
+                                          i, j, board)
+                chess_3d_corners.append(c)
 
-        # coreners are ordered row by row, left to right in every row. there are ten rows.
-        print('match index with position on board')
-        for j in range(board_y_inner_corners):
-            for i in range(board_x_inner_corners):
-                num_line = (i * board_row_direction)
-                num_column = (j * board_column_direction)
-                board_x = board_zero_x + 2.5 * num_line
-                board_y = board_zero_y + 2.5 * num_column
-                #print(f'i,j:{i},{j}: x,y: {board_x},{board_y}')
-                chess_3d_corners.append((board_x, board_y, 0))
+        second_board = 'A' if board == 'B' else 'B'
+        arena_points, point_names = get_board_visible_points(second_board, square_size)
+        chess_3d_corners.extend(arena_points)
+        points = select_points(point_names, camera, frame)
+        chess_corners.extend(points)
 
         cc = CoordinateCalculator(model_points=np.array(chess_3d_corners))
         world_to_pixels, r, t = cc.get_world_to_pixels_translation(camera, chess_corners)
-        plot_arena_axis(camera, display_frame, world_to_pixels)
-        plt.figure(figsize=(12,8))
+        plot_arena_axis(display_frame, world_to_pixels)
+        plt.figure(figsize=(12, 8))
         plt.imshow(display_frame)
         plt.show()
         arena_axis_file_name = f'arena_axis_{camera}.png'
@@ -210,7 +266,7 @@ def main():
         matrix_file_path = path.join(args.matrices_dir, f'{video.split(".")[0]}_matrix.data')
         with open(matrix_file_path, 'wb') as f:
             pickle.dump(camera_matrix, f)
-            print(f'writing matric to: {matrix_file_path}')
+            print(f'writing camera matrix to: {matrix_file_path}')
 
     print('Sanity check: printing location of cameras relatively to the arena')
     arena = load_arena(args.matrices_dir, args.timestamp)
